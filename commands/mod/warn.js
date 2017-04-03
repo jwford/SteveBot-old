53c1841338e67b91f5275e9a561b2a5b43efcd9d
jwford <jonathanwford156@gmail.com>
2017-04-03 14:07:14 -0500

    more warn functionality

--- commands/mod/warn.js
+++ commands/mod/warn.js
@@ -11,11 +11,12 @@ class WarnCommand extends commando.Command {
   }
 
   async run(message, args) {
-    console.log('You warned a user.');
+    if (!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return message.reply('you do not have this permission.');
     if (message.mentions.users.size < 1) return message.reply('You must mention a user to warn them.');
     if (message.mentions.users.size > 1) return message.reply('You can only warn one user at a time.');
     var warnedUser = message.mentions.users.first();
-    message.channel.sendMessage(warnedUser + ", this is a warning.");
+    message.channel.sendMessage(warnedUser + ", please turn the bus around. ");
+    message.delete();
   }
 }
 
