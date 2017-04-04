0af3cc304ec3389966250531d006b379cc8764a5
jwford <jonathanwford156@gmail.com>
2017-04-04 11:46:49 -0500

    new message for more than one reason

--- commands/mod/warn.js
+++ commands/mod/warn.js
@@ -16,14 +16,17 @@ class WarnCommand extends commando.Command {
     var numOfMentions = message.mentions.users.size;
     var reason = message.content.split('\"');
     if (!messageAuthor.hasPermission("MANAGE_MESSAGES")) { //check if author of command message is mod or admin
-      message.reply('you do not have this permission.');
       message.delete();
+      return message.reply('you do not have this permission.');
     } else if (numOfMentions > 1 || numOfMentions < 1) { //check that one user has been tagged
       message.delete();
       return message.reply('you must tag one user at a time to warn them.');
-    } else if (reason.length < 2) {
+    } else if (reason.length < 2) { //check that a reason has been given
       message.delete();
-      return message.reply('You must supply a reason for the warning.');
+      return message.reply('you must supply a reason for the warning.');
+    } else if (reason.length > 2) { //check that only one set of quotes was used
+      message.delete();
+      return message.reply('please only use one set of quotes for the reason.');
     }
       reason = reason[1];
       message.channel.sendMessage(warnedUser + ', please turn the bus around. ' + reason + ' :bus:');
