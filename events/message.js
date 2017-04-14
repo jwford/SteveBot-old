e048234aa96546dc0676dd64edaeea886033f768
jwford <jonathanwford156@gmail.com>
2017-04-14 00:24:41 -0400

    mention spam upped from 10 to 15

--- events/message.js
+++ events/message.js
@@ -1,3 +1,7 @@
 module.exports = message => {
-  console.log('test');
+  var numMentions = msg.mentions.users.size;
+    if (numMentions >= 15) {
+      msg.delete();
+      return msg.reply('Can you stop spamming mentions so I can go back to my eucalyptus?');
+    }
 };
