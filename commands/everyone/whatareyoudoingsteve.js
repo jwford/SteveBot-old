02e6a847418fb5ade19043d050fc43a954ea6c46
jwford <jonathanwford156@gmail.com>
2017-04-17 22:59:13 -0400

    removed one of the shirt phrases

--- commands/everyone/whatareyoudoingsteve.js
+++ commands/everyone/whatareyoudoingsteve.js
@@ -38,7 +38,6 @@ module.exports = class WhatAreYouDoingCommand extends commando.Command {
       'Writing some Koana Lisa fanfic!',
       'Trying to explain pregaroo. It\'s not working out well...',
       '<http://tuataria.com>',
-      'http://tuataria.com/shirt',
       'Reminder to drink water and :selfcarebunny:!',
       'Listening to Matilda because of Rith.',
       'Do you want to show your Tuataria pride in public? YES! Then you should go to DFTBA.com and purchase yourself an It\'s a Tuatara shirt to showcase your pride!! Don\'t forget that 15% of the proceeds goes towards Project for Awesome! Nothing is better than that!! Buy your Tuatara shirt now!! http://tuataria.com/shirt',
