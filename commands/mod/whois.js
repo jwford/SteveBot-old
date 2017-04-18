dc8ad45e1c93305ff10061796a53ea20052016c6
jwford <jonathanwford156@gmail.com>
2017-04-18 13:39:10 -0400

    added whois.js

---
+++ commands/mod/whois.js
@@ -0,0 +1,70 @@
+const commando = require('discord.js-commando');
+const discord = require('discord.js');
+
+module.exports = class WhoIsCommand extends commando.Command {
+  constructor(stevebot) {
+    super(stevebot, {
+      name: 'whois',
+      group: 'mod',
+      memberName: 'whois',
+      description: 'Displays information about a user.',
+      args: [{
+        key: 'user',
+        label: 'user',
+        prompt: 'Tag a user to get information about them.',
+        type: 'user'
+      }]
+    });
+  }
+
+  run(msg, args) {
+    var user = args.user;
+    var userAsMember = msg.guild.member(user);
+    var game = user.presence.game;
+    var userVoiceChannel = userAsMember.voiceChannel;
+    var userSelfMute;
+    var userSelfDeaf;
+    var roles = userAsMember.roles.map(r => r.name).join(', ');
+    roles = roles.slice(1);
+
+    if (!userVoiceChannel) {
+      userVoiceChannel = 'None';
+      userSelfMute = 'Not in voice';
+      userSelfDeaf = 'Not in voice';
+    } else {
+      userVoiceChannel = userVoiceChannel.name;
+      userSelfMute = userAsMember.selfMute;
+      userSelfDeaf = userAsMember.selfDeaf;
+    }
+
+    if (game === null) {
+      game = 'None';
+    } else {
+      game = game.name;
+    }
+
+
+    const embed = new discord.RichEmbed()
+    .setTitle('User Information')
+    .setThumbnail(user.displayAvatarURL)
+    .setColor(0x26d1a9)
+    .addField('Username:', user.username, true)
+    .addField('Discriminator:', user.discriminator, true)
+    .addField('ID:', user.id, true)
+    .addField('Bot:', user.bot, true)
+    .addField('Status:', user.presence.status, true)
+    .addField('Game:', game, true)
+    .addField('Display Name:', userAsMember.displayName, true)
+    .addField('Highest Role:', userAsMember.highestRole, true)
+    .addField('Voice Channel:', userVoiceChannel, true)
+    .addField('Server-Muted:', userAsMember.serverMute, true)
+    .addField('Self-Muted:', userSelfMute, true)
+    .addField('Server-Deafened:', userAsMember.serverDeaf, true)
+    .addField('Self-Deafened:', userSelfDeaf, true)
+    .addField('Roles:', roles, true)
+    .addField('Account Created:', user.createdAt, true)
+    .addField(`Joined ${msg.guild.name}:`, userAsMember.joinedAt, true)
+    .setTimestamp();
+    msg.channel.sendEmbed(embed);
+  }
+};
