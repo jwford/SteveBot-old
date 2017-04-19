d4a498e9abcab074036899f21a0531c51ab6cb36
jwford <jonathanwford156@gmail.com>
2017-04-19 08:47:39 -0400

    more emoji reactions

--- events/message.js
+++ events/message.js
@@ -10,12 +10,29 @@ module.exports = msg => {
         msg.reply('I\'m trying to eat eucalyptus in peace, so please stop mentioning everyone.');
       }
     }
+    //reactions
     var dftba = new RegExp('dftba', 'i');
+    var thankyou = new RegExp('thank you', 'i');
+    var noedge = new RegExp('no edge', 'i');
     if (dftba.test(msg.content)) {
       if (!msg.channel.guild.emojis.find('name', 'dftba')) {
         return console.log('That emoji does not exist');
       } else {
         msg.react(msg.channel.guild.emojis.find('name', 'dftba'));
+      }
+    }
+    if (thankyou.test(msg.content)) {
+      if (!msg.channel.guild.emojis.find('name', 'yourewelcome')) {
+        return console.log('That emoji does not exist');
+      } else {
+        msg.react(msg.channel.guild.emojis.find('name', 'yourewelcome'));
+      }
+    }
+    if (noedge.test(msg.content)) {
+      if (!msg.channel.guild.emojis.find('name', 'noedge')) {
+        return console.log('That emoji does not exist.');
+      } else {
+        msg.react(msg.channel.guild.emojis.find('name', 'noedge'));
       }
     }
 };
