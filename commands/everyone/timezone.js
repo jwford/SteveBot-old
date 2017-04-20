ae7c877756416f66ac4c8986c5ecb1deff1b9dde
jwford <jonathanwford156@gmail.com>
2017-04-20 01:13:52 -0400

    embeds

--- commands/everyone/timezone.js
+++ commands/everyone/timezone.js
@@ -13,7 +13,7 @@ module.exports = class TimezoneCommand extends commando.Command {
         {
           key: 'time',
           label: 'time',
-          prompt: 'What time would you like to convert?',
+          prompt: 'What time would you like to convert? 24 hour format please.',
           type: 'string'
         },
         {
@@ -48,6 +48,21 @@ module.exports = class TimezoneCommand extends commando.Command {
     var hourDifference;
     var nextDay = false;
     var prevDay =  false;
+    var used24hr = false;
+    var ampm = inputTime[1].slice(2);
+
+    if (!ampm) {
+      console.log('No ampm.');
+    } else {
+      ampm = ampm.toUpperCase();
+    }
+
+    if (inputTime.length > 5 && inputTime < 12) {
+      used24hr = true;
+      inputHours += 12;
+    }
+
+    console.log(used24hr);
 
     if (!firstZoneMinutes) {
       firstZoneMinutes = '0';
@@ -62,6 +77,13 @@ module.exports = class TimezoneCommand extends commando.Command {
     }
     if (secondZoneHour.endsWith(':')) {
       secondZoneHour = secondZoneHour.slice(0, 2);
+    }
+
+    if (!firstZoneHour) {
+      firstZoneHour = '0';
+    }
+    if (!secondZoneHour) {
+      secondZoneHour = '0';
     }
 
     firstZoneHour = parseInt(firstZoneHour, 10);
@@ -92,13 +114,14 @@ module.exports = class TimezoneCommand extends commando.Command {
     newMinutes = inputMinutes;
 
     if (firstZoneHour < secondZoneHour) {
-      hourDifference = Math.abs(firstZoneHour) + Math.abs(secondZoneHour);
+      hourDifference = Math.abs(firstZoneHour) - Math.abs(secondZoneHour);
       newMinutes += secondZoneMinutes;
     }
     if (firstZoneHour > secondZoneHour) {
       hourDifference = -Math.abs(firstZoneHour - secondZoneHour);
       newMinutes -= firstZoneMinutes;
     }
+    console.log(hourDifference);
 
     newHours = hourDifference + inputHours;
 
@@ -121,23 +144,48 @@ module.exports = class TimezoneCommand extends commando.Command {
       newMinutes = 60 - newMinutes;
     }
 
+    if (used24hr === false) {
+      if (newHours > 12) {
+        newHours -= 12;
+        ampm = 'PM';
+      } else {
+        ampm = 'AM';
+      }
+    }
+
     newHours = newHours.toString();
     newMinutes = newMinutes.toString();
 
     if (newMinutes === '0') {
       newMinutes = '00';
     }
+    if (newMinutes.length === 1) {
+      newMinutes = newMinutes.padStart(2, '0');
+    }
 
     var newTime = [newHours, newMinutes];
     newTime = newTime.join(':');
     inputTime = inputTime.join(':');
+    inputTime = inputTime.toUpperCase();
 
     if (nextDay === true) {
-      msg.channel.sendMessage(inputTime + ' in ' + firstZone + ' is ' + newTime + ' in ' + secondZone + ' on the next day.');
+       const embed = new RichEmbed()
+      .setColor(0x462fef)
+      .addField(`Time in ${firstZone}: `, inputTime, true)
+      .addField(`Time in ${secondZone}: `, newTime + ampm + ' on the next day', true);
+      msg.channel.sendEmbed(embed);
     } else if (prevDay === true) {
-      msg.channel.sendMessage(inputTime + ' in ' + firstZone + ' is ' + newTime + ' in ' + secondZone + ' on the previous day.');
+       const embed = new RichEmbed()
+      .setColor(0x462fef)
+      .addField(`Time in ${firstZone}: `, inputTime, true)
+      .addField(`Time in ${secondZone}: `, newTime + ampm + ' on the previous day', true);
+      msg.channel.sendEmbed(embed);
     } else {
-      msg.channel.sendMessage(inputTime + ' in ' + firstZone + ' is ' + newTime + ' in ' + secondZone + ' on the same day.');
+       const embed = new RichEmbed()
+      .setColor(0x462fef)
+      .addField(`Time in ${firstZone}: `, inputTime, true)
+      .addField(`Time in ${secondZone}: `, newTime + ampm + ' on the same day', true);
+      msg.channel.sendEmbed(embed);
     }
   }
 };
