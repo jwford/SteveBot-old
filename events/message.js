883237912aecad31754a3e83ca75b0d1feaf10fa
jwford <jonathanwford156@gmail.com>
2017-04-20 18:48:44 -0400

    added config.json and basic word blacklist framework

--- events/message.js
+++ events/message.js
@@ -1,3 +1,4 @@
+const config = require('../config.json');
 module.exports = msg => {
   var numMentions = msg.mentions.users.size;
   if (numMentions >= 15) {
@@ -10,11 +11,27 @@ module.exports = msg => {
       msg.reply('I\'m trying to eat eucalyptus in peace, so please stop mentioning everyone.');
     }
   }
-    //reactions
-  var dftba = new RegExp('dftba', 'i');
-  var thankyou = new RegExp('thank you', 'i');
-  var noedge = new RegExp('no edge', 'i');
-  var pizza = new RegExp('pizza', 'i');
+
+  //blacklist
+  for (var i = 0; i < config.blacklist.length; i++) {
+    var item = config.blacklist[i];
+    item = new RegExp(item, 'i');
+    if (item.test(msg.content)) {
+      if (msg.channel.name !== 'actionlog') {
+        msg.delete();
+        msg.reply('don\'t use that word please.');
+        return;
+      } else {
+        break;
+      }
+    }
+  }
+
+  //reactions
+  var dftba = /dftba/i;
+  var thankyou = /thank you/i;
+  var noedge = /no edge/i;
+  var pizza = /pizza/i;
   if (dftba.test(msg.content)) {
     if (!msg.channel.guild.emojis.find('name', 'dftba')) {
       return console.log('That emoji does not exist');
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const commando = require('discord.js-commando');
 const path = require('path');
+const config = require('./config.json');
 const stevebot = new commando.Client({
   owner: '273707798670344192',
   commandPrefix: '!',
@@ -17,7 +18,7 @@ stevebot.registry
     .registerDefaults()
     .registerCommandsIn(path.join(__dirname, 'commands'));
 
-stevebot.login('Mjk3ODkwNzQyNzcxMjUzMjU4.C8HX5Q.L0hwHVfPxYsHrGKPgVVrug7Iu58');
+stevebot.login(config.token);
 
 process.on('unhandledRejection', err => {
   console.error('Uncaught Promise Error: \n' + err.stack);
