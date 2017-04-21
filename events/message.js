420d84c0774e9b752dbb21bfa5441177beb7f6f1
jwford <jonathanwford156@gmail.com>
2017-04-21 11:28:25 -0400

    added some emoji reacts

--- events/message.js
+++ events/message.js
@@ -28,7 +28,7 @@ module.exports = msg => {
   }
 
   //reactions
-  var reactions = [/dftba/i, /thank you/i, /no edge/i, /pizza/i, /brain soup/i];
+  var reactions = [/dftba/i, /thank you/i, /thanks/i, /no edge/i, /pizza/i, /brain soup/i, /french the llama/i, /exception/i, /mongols/i];
 
   for (var r = 0; r < reactions.length; r++) {
     var react = reactions[r];
