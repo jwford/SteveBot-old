91ca1093bb2a26f851c14940d2da7d2838af16f5
jwford <jonathanwford156@gmail.com>
2017-04-23 12:00:15 -0400

    added nickname.js

---
+++ commands/admin/nickname.js
@@ -0,0 +1,49 @@
+const commando = require('discord.js-commando');
+const RichEmbed = require('discord.js').RichEmbed;
+
+module.exports = class NicknameCommand extends commando.Command {
+  constructor(stevebot) {
+    super(stevebot, {
+      name: 'nickname',
+      aliases: ['nick'],
+      group: 'admin',
+      memberName: 'nickname',
+      description: 'Changes a user\'s nickname.',
+      args: [
+        {
+          key: 'user',
+          label: 'user',
+          prompt: 'Who\'s nickname would you like to change?',
+          type: 'user'
+        },
+        {
+          key: 'nickname',
+          label: 'new nickname',
+          prompt: 'What should the user\'s new nickname be?',
+          type: 'string'
+        }
+      ]
+    });
+  }
+
+  hasPermission(msg) {
+    return msg.member.hasPermission('MANAGE_NICKNAMES');
+  }
+
+  run(msg, args) {
+    var user = args.user;
+    var newNickname = args.nickname;
+    var modlog = msg.guild.channels.find('name', 'modlog');
+
+    msg.guild.member(user).setNickname(newNickname);
+
+    if(!modlog) return console.log('No modlog');
+    const embed = new RichEmbed()
+    .setTitle('User Nickname Changed')
+    .setColor(0x00f400)
+    .addField('User: ', `${user.username}#${user.discriminator}`, true)
+    .addField('Nickname: ', newNickname, true)
+    .setFooter('ID: ' + user.id);
+    modlog.sendEmbed(embed);
+  }
+};
