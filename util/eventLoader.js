1972238b18cc9488fb694c9622412aafe87d27de
jwford <jonathanwford156@gmail.com>
2017-04-25 12:01:34 -0400

    alphabetized events

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -1,14 +1,14 @@
 const reqEvent = (event) => require(`../events/${event}`);
 module.exports = stevebot => {
-  stevebot.on('ready', () => reqEvent('ready')(stevebot));
-  stevebot.on('message', reqEvent('message'));
-  stevebot.on('messageDelete', reqEvent('messageDelete'));
-  stevebot.on('guildMemberAdd', reqEvent('guildMemberAdd'));
-  stevebot.on('guildMemberRemove', reqEvent('guildMemberRemove'));
   stevebot.on('channelCreate', reqEvent('channelCreate'));
   stevebot.on('channelDelete', reqEvent('channelDelete'));
-  stevebot.on('roleCreate', reqEvent('roleCreate'));
-  stevebot.on('roleDelete', reqEvent('roleDelete'));
+  stevebot.on('guildMemberAdd', reqEvent('guildMemberAdd'));
+  stevebot.on('guildMemberRemove', reqEvent('guildMemberRemove'));
   stevebot.on('guildMemberUpdate', reqEvent('guildMemberUpdate'));
   stevebot.on('guildUpdate', reqEvent('guildUpdate'));
+  stevebot.on('message', reqEvent('message'));
+  stevebot.on('messageDelete', reqEvent('messageDelete'));
+  stevebot.on('ready', () => reqEvent('ready')(stevebot));
+  stevebot.on('roleCreate', reqEvent('roleCreate'));
+  stevebot.on('roleDelete', reqEvent('roleDelete'));
 };
