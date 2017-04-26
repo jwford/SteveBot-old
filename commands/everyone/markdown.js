1158adbcc0ad3a3b080e4b486d75a07ffec2200b
jwford <jonathanwford156@gmail.com>
2017-04-26 10:11:31 -0400

    made it prettier

--- commands/everyone/markdown.js
+++ commands/everyone/markdown.js
@@ -19,7 +19,7 @@ module.exports = class MarkdownCommand extends Command {
 
   run(msg, args) {
     var query = args.question;
-    
+
     switch (query) {
     case 'italics':
       msg.channel.sendMessage('\`*italics*\`');
@@ -46,7 +46,7 @@ module.exports = class MarkdownCommand extends Command {
       msg.channel.sendMessage('\`__***underline bold italics***__\`');
       break;
     case '':
-      msg.channel.sendMessage('\`*italics*\` \n \n \`**bold**\` \n \n \`***bold italics***\` \n \n \`~~strikeout~~\` \n \n \`__underline__\` \n \n \`__*underline italics*__\` \n \n \`__**underline bold**__\` \n \n \`__***underline bold italics***__\` \n \n The code blocks are done with backticks. Single backtick on either side for a one line code block, three backticks for a multi-line code block.');
+      msg.channel.sendMessage('\`\`\`fix\n *italics* \n \n **bold** \n \n ***bold italics*** \n \n ~~strikeout~~ \n \n __underline__ \n \n __*underline italics*__ \n \n __**underline bold**__ \n \n __***underline bold italics***__ \n \n The code blocks are done with backticks. Single backtick on either side for a one line code block, three backticks for a multi-line code block.\`\`\`');
       break;
     default:
       msg.reply('that\'s not a markdown thing. Stop making typos before the Jonathans notice.');
