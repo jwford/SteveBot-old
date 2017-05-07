34fdad76ff394e3eb2c93bde6eae3a64969e941a
jwford <jonathanwford156@gmail.com>
2017-05-07 12:38:11 -0400

    more readability updates

--- events/debug.js
+++ events/debug.js
@@ -1,3 +1,4 @@
 module.exports = info => {
+  if (info === 'Sending heartbeat' || info === 'Heartbeat acknowledged' || info.startsWith('Authenticated using token') || info.startsWith('Connect') || info.startsWith('Using gateway') || info.startsWith('READY') || info.startsWith('Identifying') || info.startsWith('Built')) return;
   console.log(info);
 };
